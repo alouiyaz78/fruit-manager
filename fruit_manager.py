b0377863413d2b3a6cee5753868e47856d09f8db
Yazid <alouiyaz78@gmail.com>
2025-07-22 11:43:06 -0400

    Creation de l'inventaire au format json

--- fruit_manager.py
+++ fruit_manager.py
@@ -1,20 +1,27 @@
-# %%
+import json
 inventaire ={"bananes": 130, "pommes": 60, "oranges": 45, "kiwis": 20,"ananas": 60}
 
-# %%
+def ouvrir_inventaire(path="inventaire.json"):
+    
+        with open(path, 'r',encoding='utf-8') as fichier:
+            inventaire = json.load(fichier)
+        return inventaire
+            
+def ecrire_inventaire(path="inventaire.json"):
+    with open(path, 'w', encoding='utf-8') as fichier:
+        json.dump(inventaire, fichier, ensure_ascii= False,indent=4)  
 def afficher_inventaire(inventaire):
     for fruit, quantite in inventaire.items():
         print(f"{fruit.capitalize()}: {quantite} unités")
 
-# %%
+
 afficher_inventaire(inventaire)
 
-# %%
 def recolter_fruits(inventaire, fruit, quantite):
    inventaire[fruit] = inventaire.get(fruit, 0) + quantite
    print(f"Récolté {quantite} unités de {fruit}. Inventaire mis à jour.")
 
-# %%
+
 def vendre_fruits(inventaire, fruit, quantite):
     if fruit in inventaire and inventaire[fruit] >= quantite:
         inventaire[fruit] -= quantite
@@ -22,4 +29,13 @@ def vendre_fruits(inventaire, fruit, quantite):
     else:
         print(f"Impossible de vendre {quantite} unités de {fruit}. Stock insuffisant.")
 
-# %%
+
+               
+# Exemple d'utilisation
+recolter_fruits(inventaire, "bananes", 20)
+vendre_fruits(inventaire, "pommes", 10)
+afficher_inventaire(inventaire)     
+ecrire_inventaire()  # Enregistre l'inventaire dans le fichier JSON
+# Ouvrir l'inventaire depuis le fichier JSON
+inventaire = ouvrir_inventaire()
+afficher_inventaire(inventaire)  # Affiche l'inventaire chargé depuis
